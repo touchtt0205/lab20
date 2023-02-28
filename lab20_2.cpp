1f2c0413196d81b63ab7b2a73cc27a4c2fbe0a06
Your Name <you@example.com>
2023-02-28 13:42:02 +0700

    Full

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,11 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1,Rect *R2){
+    double Lx,Ly;
+    
+    Lx = min(R1->x+R1->w,R2->x+R2->w) - max(R2->x,R2->x);
+    Ly = min(R1->y,R2->y) - max(R1->y-R1->h,R2->y-R2->h);
+    if(Lx < 0 or Ly < 0) return 0;
+    else return Lx*Ly;
 }
--- lab20_3.cpp
+++ lab20_3.cpp
@@ -8,9 +8,10 @@
 using namespace std;
 
 struct student{
-
-    //Define struct student with four member (name ,id , gender, gpa);
-    
+	char name[100];
+	int id;
+	char gender;
+	float gpa;
 };
 
 struct course{
@@ -20,7 +21,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ 
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -62,10 +63,8 @@ int main(){
 	string textline;
 	
 	while(getline(student_file,textline)){
-		student s; 
-		
-		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-
+		student s;
+			sscanf(textline.c_str(),"%[^,],%d,%c,%f",s.name,&s.id,&s.gender,&s.gpa); 
 		allstudents.push_back(s); 		
 	}
 	
@@ -83,7 +82,8 @@ int main(){
 			if(textline == "> Students"){
 				state = 3;
 			}else{
-			
+
+				allcourses[allcourses.size()-1].lecture_list.push_back(textline);
 			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
 			    
 			}			
@@ -92,7 +92,7 @@ int main(){
 				state = 1;
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
+				allcourses[allcourses.size()-1].student_list.push_back(p);
 				//Append (push_back) p to student_list of the recently added course in allcourses[];
 				
 			}
